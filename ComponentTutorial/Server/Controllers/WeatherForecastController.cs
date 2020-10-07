473d92bd6008d9c3193637f4f7801f4e9e0b832f
Huu Duc Tran <thd1152018@gmail.com>
2020-10-07 21:12:06 +0700

    Component and template.

--- ComponentTutorial/Server/Controllers/WeatherForecastController.cs
+++ ComponentTutorial/Server/Controllers/WeatherForecastController.cs
@@ -17,7 +17,7 @@ namespace ComponentTutorial.Server.Controllers
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
-        private readonly ILogger<WeatherForecastController> logger;
+        private readonly ILogger<WeatherForecastController> logger ;
 
         public WeatherForecastController(ILogger<WeatherForecastController> logger)
         {
@@ -25,16 +25,22 @@ namespace ComponentTutorial.Server.Controllers
         }
 
         [HttpGet]
-        public IEnumerable<WeatherForecast> Get()
+        public ActionResult Get()
         {
             var rng = new Random();
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = rng.Next(-20, 55),
-                Summary = Summaries[rng.Next(Summaries.Length)]
-            })
-            .ToArray();
+            return
+                Ok(Enumerable.Range(1, 10)
+                    .Select(index =>
+                    {
+                        if (index <= 0) throw new ArgumentOutOfRangeException(nameof(index));
+                        return new WeatherForecast
+                        {
+                            Date = DateTime.Now.AddDays(index),
+                            TemperatureC = rng.Next(-20, 55),
+                            Summary = Summaries[rng.Next(Summaries.Length)]
+                        };
+                    })
+                    .ToArray());
         }
     }
 }
