c7310e55eede0ba461ad3b80953b6cd0cbba6bc7
TRAN HUU DUC <thd1152015@gmail.com>
2020-10-31 09:35:02 +0700

    Customer entry

--- PizzaApp/Shared/Basket.cs
+++ PizzaApp/Shared/Basket.cs
@@ -4,11 +4,11 @@ namespace LearningBlazorWeb.Shared
 {
     public class Basket
     {
-        public Customer Customer { get; set; }
+        public Customer Customer { get; set; } = new Customer();
         public List<int> Orders { get; set; } = new List<int>();
 
         public bool HasPaid { get; set; }
-        
+
         public void Add(int pizzaId)
         {
             Orders.Add(pizzaId);
